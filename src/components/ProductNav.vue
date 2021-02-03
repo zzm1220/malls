013727eedc7f6421fbcfcd7abe2a2b855f88c9b1
zhaozhimin <zhaozhimin@chinaiptv.cn>
2021-02-03 15:21:07 +0800

    product completed

--- src/components/ProductNav.vue
+++ src/components/ProductNav.vue
@@ -1,140 +1,148 @@
 <!-- 组件说明 -->
 <template>
-    <div class="nav container" :class="{'isFixed':isFixed}">
-        <div class="nav__title">
-            <span class="nav__title__name">小米8 透明探索版</span>
-            <span class="nav__title__desc">小米8 SE</span>
-            <span class="nav__title__intro">小米8</span>
-        </div>
-        <div class="nav__param">
-            <span class="nav__param__intro">概述</span>
-            <span class="nav__param__payload">参数</span>
-            <span class="nav__param__comment">用户评价</span>
-            <slot name="navBtn"></slot>
-        </div>
+  <div
+    class="nav"
+    :class="{'isFixed':isFixed}"
+  >
+    <div class="container">
+      <div class="nav__title">
+        <span class="nav__title__name">小米8 透明探索版</span>
+        <span class="nav__title__desc">小米8 SE</span>
+        <span class="nav__title__intro">小米8</span>
+      </div>
+      <div class="nav__param">
+        <span class="nav__param__intro">概述</span>
+        <span class="nav__param__payload">参数</span>
+        <span class="nav__param__comment">用户评价</span>
+        <slot name="navBtn"></slot>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'product-nav',
-        props: ['height'],
-        components: {
+export default {
+  name: 'product-nav',
+  props: ['height'],
+  components: {
 
-        },
-        data () {
-            return {
-                isFixed: false
-            };
-        },
-        computed: {
+  },
+  data () {
+    return {
+      isFixed: false
+    };
+  },
+  computed: {
 
-        },
-        methods: {
-            initHeight(){
-                let scrollVal = window.pageYOffset ||document.documentElement.scrollTop||document.body.scrollTop
-                if (scrollVal > this.height) {
-                    this.isFixed = true
-                } else {
-                    this.isFixed = false
-                }
-            }
-        },
-        mounted() {
-            window.addEventListener('scroll',this.initHeight)
-        },
-        destroyed() {
-            window.removeEventListener('scroll',this.initHeight)
-        },
+  },
+  methods: {
+    initHeight () {
+      let scrollVal = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      if (scrollVal > this.height) {
+        this.isFixed = true
+      } else {
+        this.isFixed = false
+      }
     }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.initHeight)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.initHeight)
+  },
+}
 </script>
 
 <style scoped lang="scss">
-@import "../assets/scss/base.scss";
-.nav{
+@import '../assets/scss/base.scss';
+.nav {
+  height: 71px;
+  &.isFixed {
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+  .container {
     height: 71px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    border-top: 1px solid #E5E5E5;
-    background: #ffffff;
-    &.isFixed{
-        position: fixed;
-        top: 0;
-        left: 348px;
+  }
+  border-top: 1px solid #e5e5e5;
+  background: #ffffff;
+  &__title {
+    font-size: 12px;
+    color: #666666;
+    height: 27px;
+    &__name {
+      display: inline-block;
+      height: 100%;
+      position: relative;
+      font-size: 18px;
+      color: #333333;
+      padding-right: 13px;
+      &::after {
+        position: absolute;
+        top: 6px;
+        right: 4px;
+        content: '|';
+        font-size: 11px;
+      }
     }
-    &__title {
-        font-size: 12px;
-        color: #666666;
-        height: 27px;
-        &__name {
-            display: inline-block;
-            height: 100%;
-            position: relative;
-            font-size: 18px;
-            color: #333333;
-            padding-right: 13px;
-            &::after {
-                position: absolute;
-                top: 6px;
-                right: 4px;
-                content:"|";
-                font-size: 11px;
-            }
-        }
-        &__desc {
-            display: inline-block;
-            height: 100%;
-            position: relative;
-            vertical-align: middle;
-            padding: 0 10px;
-            &::after {
-                position: absolute;
-                top: 2px;
-                right: 0px;
-                content:"|";
-                font-size: 11px;
-            }
-        }
-        &__intro {
-            display: inline-block;
-            height: 100%;
-            vertical-align: middle;
-            padding: 0 10px;
-        }
+    &__desc {
+      display: inline-block;
+      height: 100%;
+      position: relative;
+      vertical-align: middle;
+      padding: 0 10px;
+      &::after {
+        position: absolute;
+        top: 2px;
+        right: 0px;
+        content: '|';
+        font-size: 11px;
+      }
     }
-    &__param {
-        font-size: 14px;
-        color: #666666;
-        & span {
-            padding: 0 10px;
-        }
-        &__intro {
-            position: relative;
-            &::after {
-                content: '';
-                position: absolute;
-                top: 3px;
-                right: 0px;
-                font-size: 11px;
-                width: 1px;
-                height: 11px;
-                border-right: 1px solid #999999;
-            }
-        }
-        &__payload {
-            position: relative;
-            &::after {
-                content: '';
-                position: absolute;
-                top: 3px;
-                right: 0px;
-                font-size: 11px;
-                width: 1px;
-                height: 11px;
-                border-right: 1px solid #999999;
-            }
-        }
+    &__intro {
+      display: inline-block;
+      height: 100%;
+      vertical-align: middle;
+      padding: 0 10px;
     }
+  }
+  &__param {
+    font-size: 14px;
+    color: #666666;
+    & span {
+      padding: 0 10px;
+    }
+    &__intro {
+      position: relative;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 3px;
+        right: 0px;
+        font-size: 11px;
+        width: 1px;
+        height: 11px;
+        border-right: 1px solid #999999;
+      }
+    }
+    &__payload {
+      position: relative;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 3px;
+        right: 0px;
+        font-size: 11px;
+        width: 1px;
+        height: 11px;
+        border-right: 1px solid #999999;
+      }
+    }
+  }
 }
 </style>
--- src/pages/product/Product.vue
+++ src/pages/product/Product.vue
@@ -1,46 +1,323 @@
+<!--
+ * @Author: zhimin
+ * @Date: 2021-01-27 10:04:53
+ * @LastEditors: zhimin
+ * @LastEditTime: 2021-02-03 15:19:23
+ * @FilePath: \malls\src\pages\product\Product.vue
+-->
 <!-- 组件说明 -->
 <template>
-    <div class="product">
-        <ProductNav :height="12">
-            <template v-slot:navBtn>
-                <a href="javascript:;" class="btn">立即购买</a>
-            </template>
-        </ProductNav>
-        <div class="test">1111111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
-        <div class="test">111</div>
+  <div class="product">
+    <ProductNav :height="152">
+      <template v-slot:navBtn>
+        <a
+          href="javascript:;"
+          class="btn"
+        >立即购买</a>
+      </template>
+    </ProductNav>
+    <div class="product__main">
+      <h3 class="product__main__title">小米8</h3>
+      <p class="product__main__desc">8周年旗舰版</p>
+      <div class="product__main__intro">
+        <span class="item">全球首款双频 GP</span>
+        <span class="item">骁龙845</span>
+        <span class="item">AI 变焦双摄</span>
+        <span class="item">红外人脸识别</span>
+      </div>
+      <div class="product__main__price">
+        <span class="currentPrice">￥2599</span>
+        <span class="oldPrice">￥2999</span>
+      </div>
     </div>
+    <div class="product__detail">
+      <img
+        class="product__detail__img"
+        src="/imgs/product/product-bg-2.png"
+        alt="product__detail"
+      >
+    </div>
+    <div class="product__ad"></div>
+    <swiper :options="swiperOption">
+      <swiper-slide
+        v-for="(item,index) in sliderList"
+        :key="index"
+      >
+        <a :href="`/#/product/${item.id}`">
+          <img
+            :src="item.img"
+            alt="product"
+          >
+        </a>
+      </swiper-slide>
+      <!-- 如果需要分页器 -->
+      <div
+        class="swiper-pagination"
+        slot="pagination"
+      ></div>
+    </swiper>
+    <div class="product__video">
+      <h3 class="product__video__title">60帧超慢动作摄影</h3>
+      <h3 class="product__video__title">慢慢回味每一瞬间的精彩</h3>
+      <p class="product__video__desc1">后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！</p>
+      <p class="product__video__desc2">更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
+      <div
+        class="product__video__wrapper"
+        @click="showSlide=true"
+      >
+        <img
+          class="video__img"
+          src="/imgs/product/gallery-1.png"
+          alt=""
+        >
+      </div>
+      <div class="product__video__box">
+        <div
+          class="overlay"
+          v-if="showSlide"
+        ></div>
+        <div
+          class="video"
+          :class="{'slide':showSlide}"
+        >
+          <span
+            class="iconfont video__close"
+            @click="showSlide=false"
+          >&#xe647;</span>
+          <video
+            src="/imgs/product/video.mp4"
+            muted
+            autoplay
+            controls="controls"
+          >
+          </video>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import ProductNav from '../../components/ProductNav'
-    export default {
-        components: {
-            ProductNav
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.min.css'
+import ProductNav from '../../components/ProductNav'
+export default {
+  components: {
+    ProductNav,
+    Swiper,
+    SwiperSlide
+  },
+  data () {
+    return {
+      showSlide: false,
+      swiperOption: {
+        // autoplay: true,
+        loop: true,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+        slidesPerView: 3,
+        spaceBetween: 20,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicMainBullets: 2,
+        }
+      },
+      sliderList: [
+        {
+          id: '1',
+          img: '/imgs/product/gallery-2.png'
         },
-        data () {
-            return {
+        {
+          id: '2',
+          img: '/imgs/product/gallery-3.png'
+        },
+        {
+          id: '3',
+          img: '/imgs/product/gallery-4.png'
+        },
+        {
+          id: '54',
+          img: '/imgs/product/gallery-5.jpg'
+        },
+        {
+          id: '5',
+          img: '/imgs/product/gallery-6.jpg'
+        }
+      ]
+    };
+  },
+  computed: {
 
-            };
-        },
-        computed: {
+  },
+  methods: {
 
-        },
-        methods: {
-
-        },
-    }
+  },
+}
 </script>
 
 <style scoped lang="scss">
-.test {
-    height: 220px;
+@import '../../assets/scss/iconfont.scss';
+.product {
+  &__main {
+    box-sizing: border-box;
+    width: 100%;
+    height: 718px;
+    padding-top: 55px;
+    background: url('/imgs/product/product-bg-1.png') no-repeat center;
+    background-size: cover;
+    color: #333333;
+    text-align: center;
+    &__title {
+      font-size: 80px;
+      font-weight: bold;
+    }
+    &__desc {
+      margin-top: 13px;
+      height: 23px;
+      font-size: 23px;
+    }
+    &__intro {
+      width: 482px;
+      margin: 22px auto;
+      display: flex;
+      justify-content: space-around;
+      font-size: 16px;
+      .item {
+        position: relative;
+        &::after {
+          content: '|';
+          position: absolute;
+          top: 4px;
+          right: -14px;
+          font-size: 10px;
+        }
+        &:last-child::after {
+          content: '';
+        }
+      }
+    }
+    &__price {
+      margin-top: 40px;
+      .currentPrice {
+        font-size: 18px;
+        margin-right: 4px;
+      }
+      .oldPrice {
+        font-size: 15px;
+        text-decoration: line-through;
+      }
+    }
+  }
+  &__detail {
+    box-sizing: border-box;
+    width: 100%;
+    height: 480px;
+    padding: 38px 0 45px;
+    background: #ffffff;
+    &__img {
+      display: block;
+      width: 1226px;
+      height: 397px;
+      margin: 0 auto;
+      border: 1px dashed #333333;
+    }
+  }
+  &__ad {
+    width: 100%;
+    height: 638px;
+    margin-bottom: 36px;
+    background: url('/imgs/product/product-bg-3.png') no-repeat center;
+    background-size: cover;
+  }
+  .swiper-container {
+    width: 100%;
+    height: 356px;
+    .swiper-wrapper {
+      .swiper-slide {
+        width: 28%;
+        height: 356px;
+        img {
+          width: 100%;
+          height: 326px;
+        }
+      }
+    }
+  }
+  &__video {
+    box-sizing: border-box;
+    width: 100%;
+    height: 1044px;
+    padding-top: 82px;
+    margin-top: 98px;
+    background: #000000;
+    text-align: center;
+    &__title {
+      font-size: 40px;
+      color: #ffffff;
+    }
+    &__desc1 {
+      margin-top: 20px;
+      font-size: 20px;
+      color: #ffffff;
+      font-weight: bold;
+    }
+    &__desc2 {
+      font-size: 20px;
+      color: #ffffff;
+      font-weight: bold;
+    }
+    &__wrapper {
+      margin: 40px auto 0;
+      width: 1226px;
+      height: 540px;
+      img {
+        width: 100%;
+        height: auto;
+      }
+    }
+    &__box {
+      .overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background: #333333;
+        opacity: 0.4;
+        z-index: 299;
+      }
+      .video {
+        width: 1000px;
+        height: 536px;
+        position: fixed;
+        top: -50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 399;
+        opacity: 0;
+        transition: all 0.4s;
+        &.slide {
+          opacity: 1;
+          top: 50%;
+        }
+        video {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          outline: none;
+        }
+        &__close {
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          background: red;
+          border-radius: 50%;
+          color: #ffffff;
+          z-index: 499;
+        }
+      }
+    }
+  }
 }
 </style>
