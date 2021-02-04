bb5ca992faa915f3df882e1c7989981bb850549d
zhimin <1026938098@qq.com>
2021-02-04 23:29:45 +0800

    cart

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,30 +1,94 @@
 <!-- 组件说明 -->
 <template>
-    <div class="">
-        this is orderHeader
+  <div class="cart__header">
+    <div class="container">
+      <div class="container__logo">
+        <a href="/#/home">
+          <img
+            class="container__logo__img"
+            src="/imgs/logo-mi.png"
+            alt="mi-logo"
+          />
+        </a>
+      </div>
+      <div class="container__title">
+        <span class="container__title__cart">{{title}}</span>
+        <slot name="tip"></slot>
+      </div>
+      <div class="container__name">{{username}}</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'order-header',
-        components: {
-
-        },
-        data () {
-            return {
-
-            };
-        },
-        computed: {
-
-        },
-        methods: {
-
-        },
+export default {
+  name: "order-header",
+  props: {
+      title: String
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    username() {
+        return this.$store.state.username
     }
+  },
+  methods: {
+  },
+};
 </script>
 
-<style scoped lang="sass">
-
+<style scoped lang="scss">
+@import "../assets/scss/base.scss";
+.cart__header {
+  box-sizing: border-box;
+  width: 100%;
+  height: 112px;
+  border-bottom: 1px solid #ff6600;
+  background: #ffffff;
+  padding: 30px 0;
+  .container__logo {
+    display: inline-block;
+    width: 52px;
+    height: 52px;
+    a {
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      img {
+        width: 100%;
+        height: 100%;
+        vertical-align: middle;
+      }
+    }
+  }
+  .container__title {
+    display: inline-block;
+    height: 52px;
+    line-height: 52px;
+    margin-left: 53px;
+    vertical-align: middle;
+    &__cart {
+      font-size: 28px;
+      font-weight: bold;
+      color: #333333;
+      margin-right: 17px;
+    }
+    &__msg {
+      font-size: 14px;
+      color: #999999;
+    }
+  }
+  .container__name {
+    float: right;
+    width: 64px;
+    height: 60px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #666666;
+    line-height: 60px;
+  }
+}
 </style>
--- src/pages/cart/Cart.vue
+++ src/pages/cart/Cart.vue
@@ -8,33 +8,19 @@
 <!-- 组件说明 -->
 <template>
   <div class="cart">
-    <div class="cart__header">
-      <div class="container">
-        <div class="container__logo">
-          <img
-            class="container__logo__img"
-            src="/imgs/logo-mi.png"
-            alt="mi-logo"
-          >
-        </div>
-        <div class="container__title">
-          <span class="container__title__cart">我的购物车</span>
-          <span class="container__title__msg">温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算</span>
-        </div>
-        <div class="container__name">
-          zhimin
-        </div>
-      </div>
-    </div>
+    <order-header title="我的购物车">
+      <template v-slot:tip>
+        <span class="container__title__msg">
+          温馨提示：产品是否购买成功，以最终下单为准哦，请尽快结算
+        </span>
+      </template>
+    </order-header>
     <div class="cart__content">
       <div class="container">
         <div class="wrapper">
           <h3 class="header">
             <span class="inputBox">
-              <input
-                type="checkbox"
-                id="selectAll"
-              >
+              <input type="checkbox" id="selectAll" class="selectAll" />
               <label for="selectAll">全选</label>
             </span>
             <span class="name">商品名称</span>
@@ -44,7 +30,9 @@
             <span class="operate">操作</span>
           </h3>
           <div class="item">
-            <span></span>
+            <span class="">
+              <input type="checkbox" id="selectAll" class="selectAll" />
+            </span>
             <span></span>
             <span></span>
             <span></span>
@@ -58,24 +46,89 @@
 </template>
 
 <script>
-//import x from ''
+import OrderHeader from "../../components/OrderHeader";
 export default {
   components: {
-
+    OrderHeader,
   },
-  data () {
-    return {
-
-    };
+  data() {
+    return {};
   },
-  computed: {
-
-  },
-  methods: {
-
-  },
-}
+  computed: {},
+  methods: {},
+};
 </script>
 
 <style scoped lang="scss">
+@import "../../assets/scss/base.scss";
+.cart {
+  &__content {
+    box-sizing: border-box;
+    padding: 30px 0 133px;
+    background: #f5f5f5;
+    .container {
+      .wrapper {
+        box-sizing: border-box;
+        padding: 24px 43px;
+        background: #ffffff;
+        .header {
+          height: 50px;
+          display: flex;
+          font-size: 14px;
+          font-weight: bold;
+          color: #666666;
+          border-bottom: 1px solid #e5e5e5;
+          .inputBox {
+            flex-basis: 77px;
+            margin-right: 30px;
+            .selectAll {
+              position: relative;
+              width: 16px;
+              height: 16px;
+              margin-right: 16px;
+              font-size: 14px;
+              &::after {
+                content: "";
+                display: inline-block;
+                position: absolute;
+                top: 0;
+                width: 16px;
+                height: 16px;
+                text-align: center;
+                background: #ff6600;
+              }
+              &:checked::after {
+                content: "✓";
+                color: #ffffff;
+                font-size: 12px;
+                font-weight: bold;
+              }
+            }
+          }
+          .name {
+            flex-basis: 470px;
+            box-sizing: border-box;
+            padding-left: 110px;
+          }
+          .price {
+            flex-basis: 90px;
+            text-align: center;
+          }
+          .num {
+            flex-basis: 150px;
+            text-align: center;
+          }
+          .total {
+            flex-basis: 178px;
+            text-align: center;
+          }
+          .operate {
+            flex-basis: 160px;
+            text-align: center;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
